67d68271e34a99b8ce83dfe8e5e737280ca7ec52
tanatkla <70052049+61160177@users.noreply.github.com>
2021-03-17 18:32:21 +0700

    Update line.php

--- line.php
+++ line.php
@@ -26,7 +26,7 @@ if ( sizeof($request_array['events']) > 0 )
 			$reply_message = 'ชื่อของฉันคือ BOTCAT';
 		}
 	   
-	   	if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
+	   	else if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
 		   $url = 'https://covid19.th-stat.com/api/open/today';
 		   $ch = curl_init($url);
 		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
